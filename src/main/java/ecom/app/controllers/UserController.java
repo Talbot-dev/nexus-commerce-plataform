584cb6941324bef53b54e08948311d48743f82b5
JinBot31 <manucastillodev@gmail.com>
2026-02-22 12:23:35 -0500

    feature(userService update)

--- src/main/java/ecom/app/controllers/UserController.java
+++ src/main/java/ecom/app/controllers/UserController.java
@@ -3,6 +3,8 @@ package ecom.app.controllers;
 import ecom.app.models.User;
 import ecom.app.services.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -13,18 +15,37 @@ public class UserController {
 
     private final UserService userService;
 
+
+    @RequestMapping(value = "/users", method = RequestMethod.POST)
+    public ResponseEntity<String> createUserResource(@RequestBody User user){
+        return new ResponseEntity<>(userService.registerUser(user), HttpStatus.OK);
+    }
+
+
     @GetMapping("/users")
-    public List<User> fetchAllUsers(){
-        return userService.getAllUsers();
+    public ResponseEntity<List<User>> fetchAllUsers(){
+        if (userService.getAllUsers().isEmpty()) {
+            return new ResponseEntity<>(userService.getAllUsers(), HttpStatus.NOT_FOUND);
+        }
+       return new ResponseEntity<>(userService.getAllUsers(), HttpStatus.OK);
     }
 
     @GetMapping("/users/{id}")
-    public User findUser(@PathVariable Long id){
-        return userService.getSingleUser(id);
+    public ResponseEntity<User> findUser(@PathVariable Long id){
+        return userService.getSingleUser(id)
+                .map(ResponseEntity :: ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
-    @PostMapping("/users")
-    public List<User> createUserResource(@RequestBody User user){
-        return userService.registerUser(user);
+
+    @PutMapping("/users/{id}")
+    public ResponseEntity<String> updateUser(@RequestBody User user, @PathVariable Long id){
+        return userService.modifyUserInfo(user, id) ? ResponseEntity.ok().build() : ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/user/{id}C")
+    public ResponseEntity<String> deleteUser(@PathVariable Long id){
+        return userService.removeExistingUser(id) ? ResponseEntity.ok().build() : ResponseEntity.notFound().build();
+
     }
 }
--- src/main/java/ecom/app/services/UserService.java
+++ src/main/java/ecom/app/services/UserService.java
@@ -4,29 +4,49 @@ import ecom.app.models.User;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
     private Long registeredIDs = 0L;
     private final List<User> listOfRegisteredUsers = new ArrayList<>();
 
-    public List<User> getAllUsers (){
-        return listOfRegisteredUsers;
+    public List<User> getAllUsers() {
+        return listOfRegisteredUsers.isEmpty()
+                ? Collections.emptyList()
+                : listOfRegisteredUsers;
     }
 
-    public User getSingleUser(Long id){
-        for (User user : listOfRegisteredUsers){
-            if(user.getId().equals(id)){
-                return user;
-            }
-        }
-        return null;
+    public Optional<User> getSingleUser(Long id) {
+        return listOfRegisteredUsers.stream()
+                .filter(user -> user.getId().equals(id))
+                .findFirst();
     }
 
-    public List<User> registerUser(User user){
+    public String registerUser(User user){
         user.setId(registeredIDs += 1);
         listOfRegisteredUsers.add(user);
-        return listOfRegisteredUsers;
+        return "Usuario Exitosamente registrado";
+    }
+
+    public boolean modifyUserInfo(User userInfoAdded, Long id) {
+        return listOfRegisteredUsers.stream()
+                .filter(user -> user.getId().equals(id))
+                .findFirst()
+                .map(ExistingUser -> {
+                    ExistingUser.setFirstName(userInfoAdded.getFirstName());
+                    ExistingUser.setLastName(userInfoAdded.getLastName());
+                    return true;
+                }).orElse(false);
+    }
+
+    public boolean removeExistingUser(Long id) {
+        return listOfRegisteredUsers.stream()
+                .filter(user -> user.getId().equals(id))
+                .findFirst()
+                .m
+
     }
 }
