852bcbd40c6943b2c881ee521e51b465fa0dd4a2
JinBot31 <manucastillodev@gmail.com>
2026-03-29 12:37:05 -0500

    docs: add javadoc documentation to the UserRepository class

--- src/main/java/ecom/app/userModule/repository/UserRepository.java
+++ src/main/java/ecom/app/userModule/repository/UserRepository.java
@@ -4,6 +4,10 @@ import ecom.app.userModule.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * Data access layer responsible for connecting and perform transactional operations into the database
+ *
+ * */
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
